c4f662c274bb7b4bc0c1a559e597ac3d27ba3754
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2019-09-02 23:51:50 -0300

    Downsized frames and started annotation

--- convert_frames_keeping_aspect_radio.py
+++ convert_frames_keeping_aspect_radio.py
@@ -1,10 +1,25 @@
 import os
-from pil import Image
+from PIL import Image
 
 PRINTS_DICT = './prints'
 RESIZED_PRINTS_DICT = './resized_prints'
 
-for folder in os.listdir(PRINTS_DICT):
-    for image in os.listdir(folder):
+RESIZE_WIDTH = 500
+RESIZE_HEIGHT = 281
 
-        os.mkdir(os.path.join(RESIZED_PRINTS_DICT, folder))
+for folder in os.listdir(PRINTS_DICT):
+
+    original_path = os.path.join(PRINTS_DICT, folder)
+    resized_path = os.path.join(RESIZED_PRINTS_DICT, folder)
+
+    if os.path.exists(resized_path):
+        for frame in os.listdir(resized_path):
+            os.remove(os.path.join(resized_path, frame))
+        os.rmdir(resized_path)
+
+    os.makedirs(resized_path)
+
+    for image_path in os.listdir(original_path):
+        image = Image.open(os.path.join(original_path, image_path))
+        image.thumbnail((RESIZE_WIDTH, RESIZE_HEIGHT), Image.ANTIALIAS)
+        image.save(os.path.join(resized_path, image_path))
